d3f28ee5c70a3db3fcdf51309c9d4029e505db3b
zhahuan <zhahuan@didichuxing.com>
2017-05-10 20:10:40 +0800

    新增UT 写发

---
+++ unitcase-srv/src/main/java/org/unitcase/srv/App.java
@@ -0,0 +1,13 @@
+package org.unitcase.srv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
